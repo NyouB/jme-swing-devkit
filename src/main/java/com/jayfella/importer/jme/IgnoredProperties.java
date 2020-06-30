30bb5804839f55684bdf7993d59862d82bf44093
jayfella <jameskhan1983@gmail.com>
2020-06-30 09:38:06 +0100

    Add filtering and monitoring of values.

---
+++ src/main/java/com/jayfella/importer/jme/IgnoredProperties.java
@@ -0,0 +1,10 @@
+package com.jayfella.importer.jme;
+
+public class IgnoredProperties {
+
+    public static final String[] material = {
+            "ShadowMap0", "ShadowMap1", "ShadowMap2", "ShadowMap3", "ShadowMap4", "ShadowMap5",
+            "lightDir", "lightPos"
+    };
+
+}
