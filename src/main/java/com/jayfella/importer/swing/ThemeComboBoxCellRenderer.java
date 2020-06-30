da01c907fbeb83a45ea244935a0eb6b736c8a983
jayfella <jameskhan1983@gmail.com>
2020-06-30 09:38:59 +0100

    More theming additions.

---
+++ src/main/java/com/jayfella/importer/swing/ThemeComboBoxCellRenderer.java
@@ -0,0 +1,21 @@
+package com.jayfella.importer.swing;
+
+import com.github.weisj.darklaf.theme.Theme;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ThemeComboBoxCellRenderer implements ListCellRenderer<Class<? extends Theme>> {
+
+    private final DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer();
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends Class<? extends Theme>> list, Class<? extends Theme> value, int index, boolean isSelected, boolean cellHasFocus) {
+        JLabel renderer = (JLabel) defaultRenderer.getListCellRendererComponent(list, value, index,
+                isSelected, cellHasFocus);
+
+        renderer.setText(value.getSimpleName().replace("Theme", ""));
+        return renderer;
+    }
+
+}
