b4f1e741efdfb69c050cc278555e469eb585a601
jayfella <jameskhan1983@gmail.com>
2020-07-03 14:35:00 +0100

    Add user data to tree roots so we know when we've hit them in parent traversal.

--- src/main/java/com/jayfella/importer/tree/TreeConstants.java
+++ src/main/java/com/jayfella/importer/tree/TreeConstants.java
@@ -2,7 +2,12 @@ package com.jayfella.importer.tree;
 
 public class TreeConstants {
 
-    public static final String UNDELETABLE_FLAG = "DO_NOT_DELETE"; // a Spatial hint that this spatial cannot be deleted (e.g. the root nodes).
+    // Indicates that this node is a tree root. The "fake" rootNode and guiNode will be the only holders of this flag.
+    public static final String TREE_ROOT = "TREE_ROOT";
+
+    // a Spatial hint that this spatial cannot be deleted (e.g. the root nodes).
+    public static final String UNDELETABLE_FLAG = "DO_NOT_DELETE";
+
     private static final String LIGHTS_COLLECTION_NAME = "Lights"; // a Light collection list name.
     private static final String CONTROLS_COLLECTION_NAME = "Controls"; // a Control collection list name.
 
