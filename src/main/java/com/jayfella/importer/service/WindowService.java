41342adf8e6ff3630281aa6e7d6a9aecab4667c3
jayfella <jameskhan1983@gmail.com>
2020-09-07 11:03:06 +0100

    Add javadoc

--- src/main/java/com/jayfella/importer/service/WindowService.java
+++ src/main/java/com/jayfella/importer/service/WindowService.java
@@ -9,6 +9,11 @@ import java.awt.*;
  */
 public class WindowService implements Service {
 
+    /**
+     * Sets the position of a window from saved data.
+     * @param window the window to position.
+     * @param name   the name of the window.
+     */
     public void positionWindowFromSavedPosition(Window window, String name) {
             Point location = DevKitConfig.getInstance().getSdkConfig().getWindowLocation(name);
             if (location != null) {
@@ -16,6 +21,11 @@ public class WindowService implements Service {
             }
     }
 
+    /**
+     * Resizes the window from saved data.
+     * @param window the window to resize.
+     * @param name   the name of the window.
+     */
     public void sizeWindowFromSavedSize(Window window, String name) {
         Dimension dimension = DevKitConfig.getInstance().getSdkConfig().getWindowDimensions(name);
         if (dimension != null) {
