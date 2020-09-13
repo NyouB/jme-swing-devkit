febc0839edb41f6799e23c7a46517389295f1bc6
jayfella <jameskhan1983@gmail.com>
2020-09-13 10:25:16 +0100

    Add static string for devkit-specific packages.

---
+++ src/main/java/com/jayfella/importer/core/DevkitPackages.java
@@ -0,0 +1,9 @@
+package com.jayfella.importer.core;
+
+public class DevkitPackages {
+
+    public static final String DEVKIT = "devkit";
+    public static final String PLUGINS = DEVKIT + ".plugins";
+    public static final String APPSTATES = DEVKIT + ".appstates";
+
+}
